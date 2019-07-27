8707cde98388c9230191e22421fa597d130e8d5f
assane savadogo <assane.savadogo81@gmail.com>
2019-07-27 14:39:25 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -17,7 +17,7 @@ def third_challenge
       :hero_friends => []
    }, 
    :capulet => {
-      :patriarch => {
+      :patriarch =>  "Lord Capulet{
         :name => 
       },
       :matriarch => {
